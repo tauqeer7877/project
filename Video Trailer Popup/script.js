df0a99f039c71b63ec65e9d208ced795260e5066
Tauqeer <tauqeer7877@gmail.com>
2022-10-13 21:48:57 +0530

    project16

---
+++ Video Trailer Popup/script.js
@@ -0,0 +1,16 @@
+const bntE1 = document.querySelector(".btn");
+const closeIconE1 = document.querySelector(".close-icon ");
+const trailerContainerE1 = document.querySelector(".trailer-container");
+
+const videoE1 = document.querySelector("video");
+
+bntE1.addEventListener("click", ()=>{
+    trailerContainerE1.classList.remove("active")
+});
+
+closeIconE1.addEventListener("click", ()=>{
+    trailerContainerE1.classList.add("active")
+    videoE1.pause();
+    videoE1.currentTime = 0;
+    
+});
